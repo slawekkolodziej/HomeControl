59ebc30367a5c026c9c0857e1fb1144dfa1ce4c9
Slawek Kolodziej <hfrntt@gmail.com>
2017-02-09 00:14:42 +0100

    Add simple interface

--- index.ios.js
+++ index.ios.js
@@ -9,23 +9,88 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  StatusBar,
+  Slider,
+  Switch,
+  SegmentedControlIOS
 } from 'react-native';
 
 export default class HomeControl extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ac: {
+        state: 'on',
+        mode: 'auto',
+        temperature: 22,
+        fan: 0
+      }
+    };
+  }
+
+  modifyAc( key, value ) {
+    this.setState({
+      ac: {
+        ...this.state.ac,
+        [key]: value
+      }
+    });
+  }
+
   render() {
+    const { ac } = this.state;
+
+    const isAcEnabled = ac.state === 'on';
+
+    const acModes = [
+      'auto',
+      'cooling',
+      'dehumidification',
+      'heating',
+      'fan'
+    ];
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={ styles.container }>
+        <StatusBar
+          backgroundColor="blue"
+          barStyle="light-content"
+        />
+        <View style={ styles.headerContainer }>
+          <Text style={ styles.header }>
+            HomeControl
+          </Text>
+        </View>
+
+        <View style={ styles.bodyContainer }>
+          <Switch
+            onValueChange={ value => this.modifyAc( 'state', value ? 'on' : 'off' )}
+            value={ isAcEnabled } />
+          <Slider
+            step={ 1 }
+            minimumValue={ 18 }
+            maximumValue={ 30 }
+            value={ ac.temperature }
+            disabled={ !isAcEnabled }
+            onValueChange={ value => this.modifyAc( 'temperature', value ) }
+          />
+          <SegmentedControlIOS
+          disabled={ !isAcEnabled }
+            values={ acModes }
+            selectedIndex={ acModes.indexOf(ac.mode) }
+            onValueChange={ value => this.modifyAc( 'mode', value ) }
+          />
+
+          <Text>
+            Config:{ `\n` }
+            state: { `${ac.state}\n` }
+            mode: { `${ac.mode}\n` }
+            temperature: { `${ac.temperature}\n` }
+            fan: { ac.fan }
+          </Text>
+        </View>
       </View>
     );
   }
@@ -34,20 +99,27 @@ export default class HomeControl extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  headerContainer: {
+    alignSelf: 'stretch',
+    backgroundColor: '#333',
+    flexBasis: 60
+  },
+  header: {
+    color: '#fff',
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    paddingTop: 15
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  bodyContainer: {
+    alignSelf: 'stretch',
+    backgroundColor: '#ccc',
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('HomeControl', () => HomeControl);
